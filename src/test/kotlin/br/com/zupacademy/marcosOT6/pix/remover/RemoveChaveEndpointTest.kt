680fc660eb745766154413e773a94f928bc69b37
Marcos Machado <47403470+MarcosMarchado@users.noreply.github.com>
2021-08-24 13:52:39 -0300

    Testando o Registro e Exclusão de chaves Pix no BCB - Parte - 2

--- src/test/kotlin/br/com/zupacademy/marcosOT6/pix/remover/RemoveChaveEndpointTest.kt
+++ src/test/kotlin/br/com/zupacademy/marcosOT6/pix/remover/RemoveChaveEndpointTest.kt
@@ -3,17 +3,26 @@ package br.com.zupacademy.marcosOT6.pix.remover
 import br.com.zupacademy.marcosOT6.pix.PixServiceRemoverGrpc
 import br.com.zupacademy.marcosOT6.pix.RemoverChavePixRequest
 import br.com.zupacademy.marcosOT6.pix.cadastra.*
+import br.com.zupacademy.marcosOT6.pix.remover.requisicao.bcb.RemoveChavePixNoBCB
+import br.com.zupacademy.marcosOT6.pix.remover.requisicao.bcb.dto.RemoveChaveRequest
+import br.com.zupacademy.marcosOT6.pix.remover.requisicao.bcb.dto.RemoveChaveResponse
 import io.grpc.ManagedChannel
 import io.grpc.Status
 import io.grpc.StatusRuntimeException
 import io.micronaut.context.annotation.Factory
 import io.micronaut.grpc.annotation.GrpcChannel
 import io.micronaut.grpc.server.GrpcServerChannel
+import io.micronaut.http.HttpResponse
+import io.micronaut.test.annotation.MockBean
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.mockito.Mockito
+import java.time.LocalDateTime
+import java.util.*
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -26,16 +35,94 @@ internal class RemoveChaveEndpointTest() {
     @Inject
     lateinit var repository: ChaveRepository
 
+    @Inject
+    lateinit var removeChavePixNoBCB: RemoveChavePixNoBCB
+
     @BeforeEach
     fun setup(){
         repository.deleteAll()
     }
 
     @Test
-    fun `Deve remover uma chave pix existente`(){
-        //TODO: Montar cenário
+    fun `Deve remover uma chave pix existente`() {
+        //Cenário
+        val email = "marcos@gmail.com"
+        val removeRequest = RemoveChaveRequest(email, "60701190")
+        val removeResponse = RemoveChaveResponse(email, "60701190", LocalDateTime.now().toString())
+        Mockito.`when`(removeChavePixNoBCB.remove(email, removeRequest)).thenReturn(HttpResponse.ok(removeResponse))
+
+        val chave = insereChave()
+
+        val response = removeChaveClient.removerChave(
+            RemoverChavePixRequest
+                .newBuilder()
+                .setCodigoInterno(chave.codigoDoCliente)
+                .setPixId(chave.chaveId.toString())
+                .build()
+        )
+
+        with(response){
+            assertEquals(chave.chaveId, UUID.fromString(pixId))
+            assertEquals(chave.codigoDoCliente, codigoInterno)
+            assertTrue(!repository.existsByValorDaChave(chave.valorDaChave))
+        }
+    }
+
+    @Test
+    fun `Deve dar erro ao remover uma chave que nao exista`() {
+
+        val request = RemoverChavePixRequest.newBuilder()
+            .setPixId("c56dfef4-7901-44fb-84e2-a2cefb157890")
+            .setCodigoInterno("44fb-84e2-a2cefb157890")
+            .build()
+
+        val error = assertThrows<StatusRuntimeException> {
+            removeChaveClient.removerChave(request)
+        }
+
+        with(error) {
+            assertEquals(Status.NOT_FOUND.code, status.code)
+            assertEquals("Chave não encontrada.", status.description)
+        }
+    }
+
+    @Test
+    fun `Deve dar erro ao inserir codigo do cliente invalido`() {
+        val request = RemoverChavePixRequest.newBuilder()
+            .setPixId("c56dfef4-7901-44fb-84e2-a2cefb157890")
+            .setCodigoInterno("")
+            .build()
+
+        val error = assertThrows<StatusRuntimeException> {
+            removeChaveClient.removerChave(request)
+        }
+
+        with(error) {
+            assertEquals(Status.INVALID_ARGUMENT.code, status.code)
+            assertEquals("remover.codigoInterno: must not be blank", status.description)
+        }
+    }
+
+    @Test
+    fun `Deve dar erro ao inserir id da chave invalido`() {
+        val request = RemoverChavePixRequest.newBuilder()
+            .setPixId("")
+            .setCodigoInterno("c56dfef4-7901-44fb-84e2-a2cefb157890")
+            .build()
+
+        val error = assertThrows<StatusRuntimeException> {
+            removeChaveClient.removerChave(request)
+        }
+
+        with(error) {
+            assertEquals(Status.INVALID_ARGUMENT.code, status.code)
+            assertEquals("remover.pixId: must not be blank", status.description)
+        }
+    }
+
+    fun insereChave(): ChaveEntidade {
         val codigoDoCliente = "c56dfef4-7901-44fb-84e2-a2cefb157890"
-        val conta = ChaveEntidade(
+        val chave = ChaveEntidade(
             valorDaChave = "marcos@gmail.com",
             codigoDoCliente = codigoDoCliente,
             tipoDeChave = TipoDeChave.EMAIL,
@@ -49,72 +136,12 @@ internal class RemoveChaveEndpointTest() {
                 cpfDoTitular = "05477588873"
             )
         )
-
-        val contaSalva = repository.save(conta)
-
-        val request = RemoverChavePixRequest.newBuilder()
-                            .setCodigoInterno(codigoDoCliente)
-                            .setPixId(contaSalva.chaveId.toString())
-                            .build()
-
-        val response = removeChaveClient.removerChave(request)
-
-        with(response){
-            assertEquals(contaSalva.chaveId.toString(), pixId)
-            assertEquals(contaSalva.codigoDoCliente, codigoInterno)
-        }
+        return repository.save(chave)
     }
 
-    @Test
-    fun `Deve dar erro ao remover uma chave que nao exista`(){
-
-        val request = RemoverChavePixRequest.newBuilder()
-                            .setPixId("c56dfef4-7901-44fb-84e2-a2cefb157890")
-                            .setCodigoInterno("44fb-84e2-a2cefb157890")
-                            .build()
-
-        val error = assertThrows<StatusRuntimeException> {
-            removeChaveClient.removerChave(request)
-        }
-
-        with(error){
-            assertEquals(Status.NOT_FOUND.code, status.code)
-            assertEquals("Chave não encontrada.", status.description)
-        }
-    }
-
-    @Test
-    fun `Deve dar erro ao inserir codigo do cliente invalido`(){
-        val request = RemoverChavePixRequest.newBuilder()
-            .setPixId("c56dfef4-7901-44fb-84e2-a2cefb157890")
-            .setCodigoInterno("")
-            .build()
-
-        val error = assertThrows<StatusRuntimeException> {
-            removeChaveClient.removerChave(request)
-        }
-
-        with(error){
-            assertEquals(Status.INVALID_ARGUMENT.code, status.code)
-            assertEquals("remover.codigoInterno: must not be blank", status.description)
-        }
-    }
-
-    @Test
-    fun `Deve dar erro ao inserir id da chave invalido`(){
-        val request = RemoverChavePixRequest.newBuilder()
-            .setPixId("")
-            .setCodigoInterno("c56dfef4-7901-44fb-84e2-a2cefb157890")
-            .build()
-
-        val error = assertThrows<StatusRuntimeException> {
-            removeChaveClient.removerChave(request)
-        }
-
-        with(error){
-            assertEquals(Status.INVALID_ARGUMENT.code, status.code)
-            assertEquals("remover.pixId: must not be blank", status.description)
-        }
+    @MockBean(RemoveChavePixNoBCB::class)
+    fun mockRemoveChavePixNoBCB(): RemoveChavePixNoBCB {
+        return Mockito.mock(RemoveChavePixNoBCB::class.java)
     }
 
     @Factory
